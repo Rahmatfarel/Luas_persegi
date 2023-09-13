b1685fcd9714435c577d93d9ce10873840ced64c
Rahmatfarel <144860175+Rahmatfarel@users.noreply.github.com>
2023-09-13 07:24:53 +0700

    Create Luas_persegi.cpp

---
+++ Luas_persegi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main ( ) {
+	  int p, L;
+	  cout<<"MENGHITUNG LUAS PERSEGI \n";
+	  cout<<"================ \n";
+	  cout<<"masukan panjang: " ;
+	  cin>>p;
+	  cout<<"masukan lebar: ";
+	  cin>>L;
+	  cout<<"hasilnya adalah "<<5*p*L <<endl; return 0; }
